8140f3cad607f824b5e9d064bbf46e4384738e13
marco.guarino <marco.guarino@maggioli.it>
2024-07-11 08:56:02 +0200

    gestione caso con spazio

--- script_fix_onGoogle.js
+++ script_fix_onGoogle.js
@@ -1,30 +1,39 @@
-var parola = 'buco';
+function jenkins(){
+   var parola = 'buco';
 
-parola = parola.toLowerCase();
-var input = document.createElement('input');
-input.type = 'file';
-input.click();
-input.onchange = e => {
-   let file = e.target.files[0];
-   let reader = new FileReader();
-   reader.readAsText(file, 'UTF-8');
-
-   reader.onload = readerEvent => {
-      let content = readerEvent.target.result.split('\n'), indStart = 0, indEnd=0, v=0;
-      let newFileContent = '';
-      content.forEach((el) => {
-         if(el.trimLeft().startsWith('public char[] ') && el.trimRight().endsWith('];')) {
-            v = parseInt(el.match(/\[(\d+)\]/)[1]);
-            if(el.toLowerCase().indexOf(parola) >= 0) {
-               indStart += v; indEnd = indStart;
-            } else {
-               indEnd += v;
-               el += ' , ' + "startIndex = "+indStart + ", endIndex = " + indEnd +')';
-               indStart = indEnd;
-               newFileContent += el + "\n";
-            }
-         }
-      });
-      console.log(newFileContent);
+   parola = parola.toLowerCase();
+   var input = document.createElement('input');
+   input.type = 'file';
+   input.click();
+   input.onchange = e => {
+       let file = e.target.files[0];
+       let reader = new FileReader();
+       reader.readAsText(file, 'UTF-8');
+   
+       reader.onload = readerEvent => {
+           let content = readerEvent.target.result.split('\n'),
+               indStart = 0,
+               indEnd = 0,
+               v = 0;
+           let newFileContent = '';
+           content.forEach((el) => {
+               if (el.trimLeft().startsWith('public char') && el.trimRight().endsWith('];')) {
+                   v = parseInt(el.match(/\[(\d+)\]/)[1]);
+                   if (el.toLowerCase().indexOf(parola) >= 0) {
+                       indStart += v;
+                       indEnd = indStart;
+                   } else {
+                       indEnd += v;
+                       el += ' , ' + "startIndex = " + indStart + ", endIndex = " + indEnd + ')';
+                       indStart = indEnd;
+                       newFileContent += el + "\n";
+                   }
+               }
+           });
+           if (newFileContent)
+               console.log(newFileContent);
+           else
+               console.error("Il file era vuoto broh");
+       }
    }
-}
+} 
